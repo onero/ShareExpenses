9b6476ffd339516c39d1ed7368378da89d5af502
Adam Hansen <adamlars90@gmail.com>
2018-03-28 20:30:20 +0200

    Started refactoring main class

--- app/src/main/java/dk/adamino/shareexpenses/ShareExpensesMain.java
+++ app/src/main/java/dk/adamino/shareexpenses/ShareExpensesMain.java
@@ -2,18 +2,16 @@ package dk.adamino.shareexpenses;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class ShareExpensesMain extends AppCompatActivity {
 
+    private static final String TAG = "Tag";
     private Button mCalculateButton;
-    private TextView mIncomeA;
-    private TextView mExpenseA;
-    private TextView mExpenseB;
-    private TextView mIncomeB;
-    private TextView mTotalExpense;
+    private TextView mIncomeA, mExpenseA, mExpenseB, mIncomeB, mTotalExpense;
     private ExpenseCalculator mCalculator;
 
     @Override
@@ -21,31 +19,35 @@ public class ShareExpensesMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense_calculator);
 
-        mCalculateButton = (Button) findViewById(R.id.btnCalculate);
-        mIncomeA = (TextView) findViewById(R.id.incomeA);
-        mExpenseA = (TextView) findViewById(R.id.expenseA);
-        mExpenseB = (TextView) findViewById(R.id.expenseB);
-        mIncomeB = (TextView) findViewById(R.id.incomeB);
-        mTotalExpense = (TextView) findViewById(R.id.totalExpense);
+        mCalculateButton = findViewById(R.id.btnCalculate);
+        mIncomeA = findViewById(R.id.incomeA);
+        mExpenseA = findViewById(R.id.expenseA);
+        mExpenseB = findViewById(R.id.expenseB);
+        mIncomeB = findViewById(R.id.incomeB);
+        mTotalExpense = findViewById(R.id.totalExpense);
 
 
-        View.OnClickListener listener = new View.OnClickListener() {
+        View.OnClickListener onCalculateClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                double incomeA = Double.parseDouble(mIncomeA.getText().toString());
-                double incomeB = Double.parseDouble(mIncomeB.getText().toString());
-                double totalExpense = Double.parseDouble(mTotalExpense.getText().toString());
+                try {
+                    double incomeA = Double.parseDouble(mIncomeA.getText().toString());
+                    double incomeB = Double.parseDouble(mIncomeB.getText().toString());
+                    double totalExpense = Double.parseDouble(mTotalExpense.getText().toString());
 
-                mCalculator = new ExpenseCalculator(incomeA, incomeB, totalExpense);
-                String expenseA = mCalculator.getExpenseA();
-                String expenseB = mCalculator.getExpenseB();
-                mExpenseA.setText(expenseA);
-                mExpenseB.setText(expenseB);
+                    mCalculator = new ExpenseCalculator(incomeA, incomeB, totalExpense);
+                    String expenseA = mCalculator.getExpenseA();
+                    String expenseB = mCalculator.getExpenseB();
+                    mExpenseA.setText(expenseA);
+                    mExpenseB.setText(expenseB);
+                } catch (NumberFormatException nfe) {
+                    Log.e(TAG, nfe.getMessage());
+                }
 
             }
         };
 
-        mCalculateButton.setOnClickListener(listener);
+        mCalculateButton.setOnClickListener(onCalculateClickListener);
     }
 
 
